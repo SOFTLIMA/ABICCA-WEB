74ad53349fc35ec033e496ad4bb256a3e2533dcd
Jhonatan Felipe <155099370+oficialjfdev@users.noreply.github.com>
2024-10-09 17:58:25 -0300

    Adicionado componente associados

--- ABICCA-WEB/src/app/app.component.ts
+++ ABICCA-WEB/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MENUNAVComponent } from './menu-nav/menu-nav.component';
+import { ASSOCIADOSComponent } from './associados/associados.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MENUNAVComponent],
+  imports: [RouterOutlet, MENUNAVComponent, ASSOCIADOSComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ABICCA-WEB';
+  title = 'A.B.I.C.C.A';
 }
---
+++ ABICCA-WEB/src/app/associados/associados.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from "@angular/material/button";
+
+@Component({
+  selector: 'app-associados',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './associados.component.html',
+  styleUrl: './associados.component.css'
+})
+export class ASSOCIADOSComponent {
+
+}
