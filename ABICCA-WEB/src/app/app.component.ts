0f7882bbaddbbcb662cbf35625789f3f6a78a747
ryanalmeida07 <almeidaryan073@gmail.com>
2024-10-10 08:37:46 -0300

    Adicionado componente quem somos

--- ABICCA-WEB/src/app/app.component.ts
+++ ABICCA-WEB/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MENUNAVComponent } from './menu-nav/menu-nav.component';
+import { SOBREComponent } from './sobre/sobre.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MENUNAVComponent],
+  imports: [RouterOutlet, MENUNAVComponent, SOBREComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
