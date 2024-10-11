0b2f1638c95ffc7c2438bd6f652bd63d10b7ce98
ryanalmeida07 <almeidaryan073@gmail.com>
2024-10-11 16:54:36 -0300

    adicionado campo imagem

--- ABICCA-WEB/src/app/app.component.ts
+++ ABICCA-WEB/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { MENUNAVComponent } from './menu-nav/menu-nav.component';
 import { SOBREComponent } from './sobre/sobre.component';
 import { ASSOCIADOSComponent } from './associados/associados.component';
+import { ImagensComponent } from './imagens/imagens.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MENUNAVComponent, SOBREComponent, ASSOCIADOSComponent],
+  imports: [RouterOutlet, ImagensComponent, MENUNAVComponent, SOBREComponent, ASSOCIADOSComponent, RodapeComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
