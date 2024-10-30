63661066202d5d23aa804d2a062b7ac18a39b60e
Jhonatan Felipe <155099370+oficialjfdev@users.noreply.github.com>
2024-10-30 11:48:17 -0300

    Substituição das credenciais por api URL lambda

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,7 +1,6 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import { Amplify } from 'aws-amplify';
 
 const serverConfig: ApplicationConfig = {
   providers: [
