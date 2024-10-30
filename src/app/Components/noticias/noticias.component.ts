2908b47c30c95e7f61f245766e8f1247d2246427
Jhonatan Felipe <155099370+oficialjfdev@users.noreply.github.com>
2024-10-30 16:34:24 -0300

    Melhorado popup de noticia de forma mais dinâmica

--- src/app/Components/noticias/noticias.component.ts
+++ src/app/Components/noticias/noticias.component.ts
@@ -16,45 +16,59 @@ interface pagination {
   styleUrl: './noticias.component.css'
 })
 export class NoticiasComponent implements OnInit{
-  DATA : CampoPainel[] = [];
-  listPagnation : pagination[] = [];
+DATA : CampoPainel[] = [];
+listPagnation : pagination[] = [];
+constructor( private ddb : DynamoDBService){}
 
-  constructor( private ddb : DynamoDBService){}
 
-
-  async ngOnInit(): Promise<void> {
-    await this.ddb.readAllItens().subscribe(result => {
-      if (result) {
-        result.forEach(item => {
-          this.DATA.push({
-            id: item['id'],
-            titulo: item['titulo'],
-            data: item['data'],
-            descricao: item['descricao'],
-            link_Imgs: item['link_Imgs'],
-          });
+async ngOnInit(): Promise<void> {
+  await this.ddb.readAllItens().subscribe(result => {
+    if (result) {
+      result.forEach(item => {
+        this.DATA.push({
+          id: item['id'],
+          titulo: item['titulo'],
+          data: item['data'],
+          descricao: item['descricao'],
+          link_Imgs: item['link_Imgs'],
         });
-
-
-        if (this.DATA.length > 0) {
-          let paginas : number = Math.ceil(this.DATA.length/4);
-
-          for (let i = 0; i <= paginas; i++) {
-            if (i > 1) {
-              this.listPagnation.push({pag: i});
-            }
-          }
-        };
-      }
+      });
+    // Ordenar a lista DATA pelo campo 'data' no formato DD/MM/YYYY
+    this.DATA.sort((a: CampoPainel, b: CampoPainel) => {
+      const dateA = this.convertToDate(a.data);
+      const dateB = this.convertToDate(b.data);
+      return dateB.getTime() - dateA.getTime(); // Para crescente (mais antiga para mais recente)
     });
-  }
-
-  showItens(): boolean{
-    if (this.DATA.length > 0) {
-      return true;
+      if (this.DATA.length > 0) {
+        let paginas : number = Math.ceil(this.DATA.length/4);
+        for (let i = 0; i <= paginas; i++) {
+          if (i > 1) {
+            this.listPagnation.push({pag: i});
+          }
+        }
+      };
     }
-    return false;
+  });
+}
+showItens(): boolean{
+  if (this.DATA.length > 0) {
+    return true;
   }
+  return false;
+}
+
+  // Método para converter data no formato DD/MM/YYYY para um objeto Date
+private convertToDate(dateString: string): Date {
+  const parts = dateString.split('/');
+  // Verifique se o formato está correto
+  if (parts.length === 3) {
+    const day = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10) - 1; // Mês começa do zero
+    const year = parseInt(parts[2], 10);
+    return new Date(year, month, day);
+  }
+  return new Date(); // Retorna uma data padrão se o formato estiver errado
+}
 
 
 }
--- src/app/Components/noticias/popup-noticia/popup-noticia.component.ts
+++ src/app/Components/noticias/popup-noticia/popup-noticia.component.ts
@@ -1,27 +1,44 @@
-import { Component, Inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-popup-noticia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup-noticia.component.html',
-  styleUrl: './popup-noticia.component.css'    
+  styleUrl: './popup-noticia.component.css'
 })
 
-export class PopupNoticiaComponent {
+export class PopupNoticiaComponent implements AfterViewInit{
 
   data : any;
 
-  constructor(
+  @ViewChild('descricaoRef', { static: false }) descricaoRef!: ElementRef;
+  isScrollable: boolean = false;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef,
     public dialogRef: MatDialogRef<PopupNoticiaComponent>,
     @Inject(MAT_DIALOG_DATA) public item: any // Recebe os dados do item
   ) {
     if (item) {
       this.data = item;
       // this.data.descricao = item.descricao.replace(/\n\n/g, "<br>")
-      
+
     }
+  }
+
+  ngAfterViewInit() {
+    const element = this.descricaoRef.nativeElement;
+    this.isScrollable = element.scrollHeight > element.clientHeight;
+
+    // Adiciona a classe scrollable se necessário
+    if (this.isScrollable) {
+      element.classList.add('scrollable');
+    }
+
+    // Notifica o Angular sobre a mudança
+    this.changeDetectorRef.detectChanges();
   }
 
 }
