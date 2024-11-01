fcf01abd1ebf79b7f3f282b2ce35f9eb87277170
Jhonatan Felipe <155099370+oficialjfdev@users.noreply.github.com>
2024-11-01 15:14:31 -0300

    Atualização de acordo com a identidade visual da ABICCA

--- src/app/Components/associados/associados.component.ts
+++ src/app/Components/associados/associados.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from "@angular/material/button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-associados',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, CommonModule],
   templateUrl: './associados.component.html',
   styleUrl: './associados.component.css'
 })
