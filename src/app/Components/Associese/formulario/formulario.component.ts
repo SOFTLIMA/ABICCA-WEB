a920f8e3392d4c2c99db4c846243ebf36b56c23b
Jhonatan Felipe <155099370+oficialjfdev@users.noreply.github.com>
2024-11-08 15:27:06 -0300

    Modificações para subir o site para a LocaWeb

--- src/app/Components/Associese/formulario/formulario.component.ts
+++ src/app/Components/Associese/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formulario',
@@ -7,6 +7,19 @@ import { Component } from '@angular/core';
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit{
+  nextUrl: string = ''; // Variável para o valor do _next
+
+  constructor() {}
+
+  ngOnInit() {
+    // Definindo a URL de redirecionamento dinamicamente
+    this.nextUrl = this.getDynamicRedirectUrl(); // Aqui você define sua lógica para gerar a URL
+  }
+
+  getDynamicRedirectUrl(): string {
+    // Lógica para criar o link de redirecionamento
+    return window.location.href; // Exemplo simples de adicionar um sufixo ao URL atual
+  }
 
 }
--- src/app/Components/contato/entre-em-contato/entre-em-contato.component.ts
+++ src/app/Components/contato/entre-em-contato/entre-em-contato.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-entre-em-contato',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
   templateUrl: './entre-em-contato.component.html',
   styleUrl: './entre-em-contato.component.css'
 })
-export class EntreEmContatoComponent {
+export class EntreEmContatoComponent implements OnInit{
+  nome: string = '';
+  subject: string = '';
+  nextUrl: string = ''; // Variável para o valor do _next
+
+  constructor() {}
+
+  ngOnInit() {
+    // Definindo a URL de redirecionamento dinamicamente
+    this.nextUrl = this.getDynamicRedirectUrl(); // Aqui você define sua lógica para gerar a URL
+    this.subject = 'Entrou em contato com a ABICCA';
+  }
+
+  getDynamicRedirectUrl(): string {
+    // Lógica para criar o link de redirecionamento
+    return window.location.href; // Exemplo simples de adicionar um sufixo ao URL atual
+  }
+
+  onSubmit() {
+    this.subject = `${this.nome}${this.subject}`;
+  }
 
 }
