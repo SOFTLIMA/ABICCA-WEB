b631c3f5b678d1b01a61fa7a1fd3269b8bd85b60
ryanalmeida07 <almeidaryan073@gmail.com>
2024-11-12 15:29:54 -0300

    Modificado  responsividade da página noticias e formulario

--- src/app/Components/noticia/noticia.component.ts
+++ src/app/Components/noticia/noticia.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CampoPainel } from '../../../Model/PainelADM';
+import { DynamoDBService } from '../../../aws/DynamoDBService';
 
 @Component({
   selector: 'app-noticia',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './noticia.component.html',
   styleUrl: './noticia.component.css'
 })
 export class NoticiaComponent {
-  descricao: string = ' A ABICCA e a ABNT selaram um acordo histórico que promete revolucionar a normalização de cabos de aço e acessórios no Brasil. Marcos Antonio Piccoli, presidente da ABICCA, e líderes da ABNT uniram forças com um objetivo claro: elevar os padrões de qualidade e segurança na indústria. Este compromisso não só reforça a competitividade do setor, mas também traz benefícios diretos para consumidores e toda a cadeia produtiva. O que mais essa parceria pode trazer para o futuro da indústria brasileira?';
+  DATA : CampoPainel[] = [];
+
+  constructor( private ddb : DynamoDBService){}
+
+  async ngOnInit(): Promise<void> {
+    await this.ddb.readAllItens().subscribe(result => {
+      if (result) {
+        result.forEach(item => {
+          this.DATA.push({
+            id: item['id'],
+            titulo: item['titulo'],
+            data: item['data'],
+            descricao: item['descricao'],
+            link_Imgs: item['link_Imgs'],
+          });
+        });
+      // Ordenar a lista DATA pelo campo 'data' no formato DD/MM/YYYY
+      this.DATA.sort((a: CampoPainel, b: CampoPainel) => {
+        const dateA = this.convertToDate(a.data);
+        const dateB = this.convertToDate(b.data);
+        return dateB.getTime() - dateA.getTime(); // Para crescente (mais antiga para mais recente)
+      });
+      }
+    });
+  }
+
+
+  private convertToDate(dateString: string): Date {
+    const parts = dateString.split('/');
+    // Verifique se o formato está correto
+    if (parts.length === 3) {
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1; // Mês começa do zero
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+    }
+    return new Date(); // Retorna uma data padrão se o formato estiver errado
+  }  
 }
